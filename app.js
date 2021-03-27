5c2add071e589b8d0f8e608f6edbece9fd9da792
dymafr <contact@dyma.fr>
2021-03-27 16:10:28 +0100

    maj

--- app.js
+++ app.js
@@ -2,31 +2,32 @@ const path = require('path');
 const express = require('express');
 require('./database');
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+const { clientPromise } = require('./database');
 const app = express();
-
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({
-  secret: 'cersei',
-  resave: false,
-  name: 'jesuisunid',
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24 * 14 // session 14 days
-  },
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 60 * 60 * 24 * 14
+app.use(
+  session({
+    secret: 'cersei',
+    resave: false,
+    name: 'jesuisunid',
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 14, // session 14 days
+    },
+    store: MongoStore.create({
+      clientPromise: clientPromise.then((m) => m.connection.getClient()),
+      ttl: 60 * 60 * 24 * 14,
+    }),
   })
-}));
+);
 
-app.get('/',  (req, res) => {
+app.get('/', (req, res) => {
   if (req.session.views) {
     req.session.views += 1;
   } else {
--- database/index.js
+++ database/index.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/test?retryWrites=true', {
-  useNewUrlParser: true,
-}).then( () => {
-  console.log('connexion ok !');
-}).catch( err => {
-  console.log(err);
-})
+exports.clientPromise = mongoose
+  .connect(
+    'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/test?retryWrites=true',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .catch((err) => {
+    console.log(err);
+  });
