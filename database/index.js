8647e619379b18423a148d8c080d6ae1555c0d05
dymafr <contact@dyma.fr>
2022-11-27 14:46:07 +0100

    maj bootstrap 5

--- database/index.js
+++ database/index.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 exports.clientPromise = mongoose
   .connect(
-    'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/test?retryWrites=true',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
+    'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/test?retryWrites=true'
   )
   .catch((err) => {
     console.log(err);
