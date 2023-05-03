3cbaaff64e5a80f54c907495aff3766a0457e9a2
dymafr <contact@dyma.fr>
2023-05-03 12:03:21 +0200

    maj mongoose 7

--- database/index.js
+++ database/index.js
@@ -4,6 +4,10 @@ exports.clientPromise = mongoose
   .connect(
     'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/test?retryWrites=true'
   )
+  .then((client) => {
+    console.log('Connected to MongoDB');
+    return client;
+  })
   .catch((err) => {
     console.log(err);
   });
